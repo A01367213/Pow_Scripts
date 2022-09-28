11826b994920bb9bf058e3b3f9b2b69feff9b610
A01367213 <destroyer_all@hotmail.es>
2022-09-28 14:37:18 -0500

    Changes in 'rename.py'
    The script now asks for the path where the name of the archives are going to be changed

--- rename.py
+++ rename.py
@@ -1,13 +1,13 @@
 import os
 
+print("Insert a '\\' at the end of the path")
+folder = input("Insert Path:") #Variable where folder path will be stored
+new_name = input("Name to be replaced: ") #Stores the new name for archives
 
-iso_8 = 'ISO-8'
-iso_9 = 'ISO-9'
-
-folder = r'D:\Operator_Day\21-09-2022_OD\\' + iso_9 + '\\'
-
+#Retrive the archive names inside a folder
 archive_name = os.listdir(folder)
 
+#Counters for name
 count_img = 1
 count_vid = 1
 
@@ -18,7 +18,7 @@ for i in range(len(archive_name)):
     if name_aux[1] == 'jpg':
         try: 
             original_file = folder + archive_name[i]
-            new_file = folder + iso_9 + '_IMG' + ('0' * (2 - len(str(count_img)))) + str(count_img) +  '.jpg'
+            new_file = folder + new_name + '_IMG' + ('0' * (2 - len(str(count_img)))) + str(count_img) +  '.jpg'
             os.rename(original_file, new_file)
             count_img += 1
 
@@ -28,7 +28,7 @@ for i in range(len(archive_name)):
     if name_aux[1] == 'mp4':
         try: 
             original_file = folder + archive_name[i]
-            new_file = folder + iso_9 + '_VID' + ('0' * (2 - len(str(count_vid)))) + str(count_vid) + '.mp4'
+            new_file = folder + new_name + '_VID' + ('0' * (2 - len(str(count_vid)))) + str(count_vid) + '.mp4'
             os.rename(original_file, new_file)
             count_vid += 1
         except:
